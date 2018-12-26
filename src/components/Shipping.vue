20601163f5685cc0d65b05afd7cc51c1d0bc5319
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-26 16:46:21 -0600

    Created Shipping Vue component

---
+++ src/components/Shipping.vue
@@ -0,0 +1,13 @@
+<template>
+    <form>
+        <form-element name="email" type="text" required="true"></form-element>
+    </form>
+</template>
+
+<script>
+import FormElement from './utilities/FormElement.vue'
+
+export default {
+  components: [ FormElement ]
+}
+</script>
