b3f0983b47067f815c24692fb032bbd41e41208f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-27 11:55:39 -0600

    Added Bootstrap form classes to FormElement elements

--- src/components/utilities/FormElement.vue
+++ src/components/utilities/FormElement.vue
@@ -1,11 +1,9 @@
 <template>
-    <div>
-        <div :class="name">
-            <label :for="name"><slot></slot></label>
+    <div class="form-group" :id="name">
+        <label :for="name"><slot></slot></label>
 
-            <input v-if="this.required" :type="type" :name="name" :id="name" required>
-            <input v-else :type="type" :name="name" :id="name">
-        </div>
+        <input v-if="this.required" class="form-control" :type="type" :name="name" :id="name" required>
+        <input v-else class="form-control" :type="type" :name="name" :id="name">
     </div>
 </template>
 
