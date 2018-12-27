de59e64a5e52a94ae2b5dc4f84c0b9b80c7653a0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-27 13:39:48 -0600

    Added header and footer to Order component

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <vue-header>Order</vue-header>
+    <vue-footer></vue-footer>
+  </div>
 </template>
+
+<script>
+import VueHeader from './utilities/VueHeader.vue'
+import VueFooter from './utilities/VueFooter.vue'
+
+export default {
+  components: { VueHeader, VueFooter }
+}
+</script>
