632854a2a4b97a2579347a8029eb1a1e82c68992
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-27 12:21:23 -0600

    Added 1em padding to top of VueHeader element

--- src/components/utilities/VueHeader.vue
+++ src/components/utilities/VueHeader.vue
@@ -1,8 +1,15 @@
 <template>
-  <header><h1><slot></slot></h1></header>
+  <header>
+    <div class="row">
+      <h1 class="col"><slot></slot></h1>
+    </div>
+  </header>
 </template>
 
 <style>
+header {
+  padding-top: 1em;
+}
 h1 {
   font: 2em;
   text-align: center;
