87a3f095a7f6e486dd1ec01de3128d577881daf2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-27 12:56:35 -0600

    Created VueFooter component

---
+++ src/components/utilities/VueFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="border-top">
+    <div class="row">
+      <ul class="col">
+        <li>&copy; Skelpo {{ year }}</li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style>
+footer {
+  margin-top: 1em;
+  padding-bottom: 1em;
+}
+
+ul {
+  text-align: center;
+  margin: 1em 0;
+}
+
+ul > li {
+  display: inline-block;
+  appearance: none;
+}
+</style>
