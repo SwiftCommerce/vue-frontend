6769fafb3b855969ccd865ce959f5a5aceb83e2e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-27 12:10:35 -0600

    Completed Shipping component form

--- src/components/Shipping.vue
+++ src/components/Shipping.vue
@@ -1,25 +1,29 @@
 <template>
-  <form>
-    <form-element name="email" required>Email Address:</form-element>
-    <form-element name="firstname" required>First Name:</form-element>
-    <form-element name="lastname" required>Last Name:</form-element>
-    <form-element name="company">Company:</form-element>
-    <form-element name="address_1" required>1st Street Address:</form-element>
-    <form-element name="address_2">2nd Street Address:</form-element>
-    <form-element name="city" required>City:</form-element>
-    <form-element name="state" required>State/Province:</form-element>
-    <form-element name="zip" required>Zip/Postal Code:</form-element>
-    <form-element name="country" required>Country:</form-element>
-    <form-element name="phone_number">Phone Number:</form-element>
+  <div>
+    <vue-header>Shipping</vue-header>
+    <form>
+      <form-element name="email" required>Email Address:</form-element>
+      <form-element name="firstname" required>First Name:</form-element>
+      <form-element name="lastname" required>Last Name:</form-element>
+      <form-element name="company">Company:</form-element>
+      <form-element name="address_1" required>1st Street Address:</form-element>
+      <form-element name="address_2">2nd Street Address:</form-element>
+      <form-element name="city" required>City:</form-element>
+      <form-element name="state" required>State/Province:</form-element>
+      <form-element name="zip" required>Zip/Postal Code:</form-element>
+      <form-element name="country" required>Country:</form-element>
+      <form-element name="phone_number">Phone Number:</form-element>
 
-    <button>Next</button>
-  </form>
+      <button type="button" class="btn btn-info">Next</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import VueHeader from './utilities/VueHeader.vue'
 import FormElement from './utilities/FormElement.vue'
 
 export default {
-  components: { FormElement }
+  components: { VueHeader, FormElement }
 }
 </script>
