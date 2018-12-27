c0c3e7cb71340603fb27c0a621a7e872fd6c9e7f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-27 10:34:55 -0600

    Completed Shipping component form elements

--- src/components/Shipping.vue
+++ src/components/Shipping.vue
@@ -1,7 +1,19 @@
 <template>
-    <form>
-        <form-element name="email" required>Email Address:</form-element>
-    </form>
+  <form>
+    <form-element name="email" required>Email Address:</form-element>
+    <form-element name="firstname" required>First Name:</form-element>
+    <form-element name="lastname" required>Last Name:</form-element>
+    <form-element name="company">Company:</form-element>
+    <form-element name="address_1" required>1st Street Address:</form-element>
+    <form-element name="address_2">2nd Street Address:</form-element>
+    <form-element name="city" required>City:</form-element>
+    <form-element name="state" required>State/Province:</form-element>
+    <form-element name="zip" required>Zip/Postal Code:</form-element>
+    <form-element name="country" required>Country:</form-element>
+    <form-element name="phone_number">Phone Number:</form-element>
+
+    <button>Next</button>
+  </form>
 </template>
 
 <script>
