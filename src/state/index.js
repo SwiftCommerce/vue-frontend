f530d9a1673e2717411c5ea6d18e6ac99bf11657
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-28 10:52:08 -0600

    Added 'categories' mutation to Vuex.Store instance

--- src/state/index.js
+++ src/state/index.js
@@ -4,5 +4,10 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {}
+  state: {},
+  mutations: {
+    categories (state, categories) {
+      state.categories = categories
+    }
+  }
 })
