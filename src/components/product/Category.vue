9f173844e2259c24896ae0faa775d7275cd9f11d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-28 13:01:28 -0600

    Created Category component for displaying products in a category

---
+++ src/components/product/Category.vue
@@ -0,0 +1,72 @@
+<template>
+  <page :name="pageName">
+
+    <div v-if="error" id="error">
+      <div class="alert alert-danger" role="alert">
+        <p>{{ error }}</p>
+      </div>
+    </div>
+
+    <div v-else-if="products.length === 0" id="no-products">
+      <div class="alert alert-warning" role="alert">
+        <p>No products found in category '{{ $route.params.category }}'</p>
+      </div>
+    </div>
+
+    <div v-else-if="products" id="products">
+      <ul>
+        <li v-for="product in products" :key="product.sku">
+          <div class="media">
+            <!-- <img src="..." class="mr-3" alt="..."> -->
+            <div class="media-body">
+              <h5 class="mt-0">{{ product.name }}</h5>
+              <p>{{ product.description }}</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+  </page>
+</template>
+
+<script>
+import Page from '@/components/page/Page.vue'
+
+function uppercasedName (vue) {
+  var name = vue.$route.params.category
+  return name.charAt(0).toUpperCase() + name.slice(1)
+}
+
+export default {
+  components: { Page },
+  data: function () {
+    return {
+      pageName: uppercasedName(this),
+      products: [],
+      error: null,
+      loading: true,
+      page: 1
+    }
+  },
+  created: function () {
+    this.getProducts()
+  },
+  methods: {
+    getProducts: function () {
+      this.error = null
+      this.loading = true
+
+      this.$api.products.get(`?categories[]=${this.$route.params.category}&page=${this.page}&pageSize=3`).then((response) => {
+        Array.prototype.push.apply(this.products, response.data.products)
+        this.page += 1
+
+        // We force update because Vue does not make the prototype functions reactive.
+        this.$forceUpdate()
+      }).catch((error) => {
+        this.error = error
+      })
+    }
+  }
+}
+</script>
