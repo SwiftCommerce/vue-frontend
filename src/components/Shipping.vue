deb90cf82b55c605f8134e41b65ae716cd5be34f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-28 11:52:28 -0600

    Moved shipping address from to AddressForm component

--- src/components/Shipping.vue
+++ src/components/Shipping.vue
@@ -1,33 +1,14 @@
 <template>
   <page name="Shipping">
-    <form>
-      <form-element name="email" required>Email Address:</form-element>
-      <form-element name="firstname" required>First Name:</form-element>
-      <form-element name="lastname" required>Last Name:</form-element>
-      <form-element name="company">Company:</form-element>
-      <form-element name="address_1" required>1st Street Address:</form-element>
-      <form-element name="address_2">2nd Street Address:</form-element>
-      <form-element name="city" required>City:</form-element>
-      <form-element name="state" required>State/Province:</form-element>
-      <form-element name="zip" required>Zip/Postal Code:</form-element>
-      <form-element name="country" required>Country:</form-element>
-      <form-element name="phone_number">Phone Number:</form-element>
-
-      <button type="submit" class="btn btn-info" @click="submit()">Next</button>
-    </form>
+    <address-form next="Order"></address-form>
   </page>
 </template>
 
 <script>
 import Page from '@/components/page/Page.vue'
-import FormElement from '@/components/utilities/FormElement.vue'
+import AddressForm from '@/components/order/AddressForm.vue'
 
 export default {
-  components: { Page, FormElement },
-  methods: {
-    submit: function () {
-      this.$router.push({name: 'Order'})
-    }
-  }
+  components: { Page, AddressForm },
 }
 </script>
---
+++ src/components/order/AddressForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+      <form-element name="email" required>Email Address:</form-element>
+      <form-element name="firstname" required>First Name:</form-element>
+      <form-element name="lastname" required>Last Name:</form-element>
+      <form-element name="company">Company:</form-element>
+      <form-element name="address_1" required>1st Street Address:</form-element>
+      <form-element name="address_2">2nd Street Address:</form-element>
+      <form-element name="city" required>City:</form-element>
+      <form-element name="state" required>State/Province:</form-element>
+      <form-element name="zip" required>Zip/Postal Code:</form-element>
+      <form-element name="country" required>Country:</form-element>
+      <form-element name="phone_number">Phone Number:</form-element>
+
+      <button v-if="next" type="submit" class="btn btn-info" @click="submit()">Next</button>
+    </form>
+</template>
+
+<script>
+import FormElement from '@/components/utilities/FormElement.vue'
+
+export default {
+  components: { FormElement },
+  props: {
+    next: { type: String }
+  },
+  methods: {
+    submit: function () {
+      if (this.next) {
+        this.$router.push({name: this.next})
+      }
+    }
+  }
+}
+</script>
