ea81173f96c132df9cadfceec23c9dad76a2922b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-28 13:02:02 -0600

    Added Category component as page for /categories/:category route path

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import HelloWorld from '@/components/HelloWorld'
+import Category from '@/components/product/Category'
 import Shipping from '@/components/order/Shipping'
 import Order from '@/components/order/Order'
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/categories/:category',
+      name: 'Category',
+      component: Category
     },
     {
       path: '/shipping',
