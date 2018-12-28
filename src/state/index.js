a58181787ebdbf4a1c7390de3e6559f717d561b3
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-28 10:56:44 -0600

    Added 'categories' property to Vuex.Store.state object

--- src/state/index.js
+++ src/state/index.js
@@ -4,7 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    categories: null
+  },
   mutations: {
     categories (state, categories) {
       state.categories = categories
