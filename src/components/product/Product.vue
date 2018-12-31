33fb5cfc001dc002342200b6dc7e2a00d531bb21
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-31 17:17:49 -0600

    Get product data from Vuex store if possible, otherwise we make an API request

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,5 +1,5 @@
 <template>
-  <page name="Product Name">
+  <page :name="product.name ? product.name : ''">
 
   </page>
 </template>
@@ -8,6 +8,28 @@
 import Page from '@/components/page/Page'
 
 export default {
-  components: { Page }
+  components: { Page },
+  data: function () {
+    return {
+      product: this.$store.state.product,
+      error: null,
+      loading: true
+    }
+  },
+  created: function () {
+    let sku = this.$route.params.product
+    if (this.product.sku === sku) {
+      this.loading = false
+      return
+    }
+
+    this.$api.products.get(`?sku=${sku}`).then((response) => {
+      this.product = response.data.products[0]
+      this.loading = false
+    }).catch((error) => {
+      this.error = error
+      this.loading = false
+    })
+  }
 }
 </script>
