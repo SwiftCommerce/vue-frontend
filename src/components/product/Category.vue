c6448c4e6fee4845fbb722f3c43bc44f68c4b116
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-31 07:29:05 -0600

    Show spinner activity indicator when Category component is loading data

--- src/components/product/Category.vue
+++ src/components/product/Category.vue
@@ -26,6 +26,14 @@
       </ul>
     </div>
 
+    <div v-if="loading">
+      <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+    </div>
+
   </page>
 </template>
 
