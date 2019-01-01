ae5cb35dc9b2f9838dc8c45bb4beb4297fb3b698
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-01 15:48:21 -0600

    Created basic Cart component for /cart route

--- src/components/product/Cart.vue
+++ src/components/product/Cart.vue
@@ -2,14 +2,41 @@
   <page name="Cart">
     <category-nav></category-nav>
 
+    <div v-if="$store.state.cart.length > 0" id="checkout" class="row">
+      <div class="col-12">
+        <button type="button" class="btn btn-primary">Checkout</button>
+      </div>
+    </div>
+    <div v-else class="row">
+      <div class="alert alert-warning" role="alert">
+        <p>You have not items in your cart. Go to our store to continue shopping!</p>
+      </div>
+    </div>
+
+    <div class="list-group list-group-flush">
+      <div v-for="item in $store.state.cart" :key="item.product.sku" class="list-group-item row">
+        <div class="col-6 float-left">
+          <h5>{{ item.product.name }}</h5>
+        </div>
+        <div class="col-6 float-right">
+          <input v-model="item.count" type="number" class="form-control" aria-label="Quantity" aria-describedby="basic-addon1">
+        </div>
+      </div>
+    </div>
   </page>
 </template>
 
 <script>
 import Page from '@/components/page/Page'
-import CategpryNav from '@/components/page/CategoryNav'
+import CategoryNav from '@/components/page/CategoryNav'
 
 export default {
-  components: { Page, CategpryNav }
+  components: { Page, CategoryNav }
 }
 </script>
+
+<style>
+#checkout {
+  margin-bottom: 1em;
+}
+</style>
