7b87fdbd3ab2db49039f597d71fbf0f5c5ea1e24
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-01 14:43:17 -0600

    Created route at /cart using Cart component

---
+++ src/components/product/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+  <page name="Cart">
+    <category-nav></category-nav>
+
+  </page>
+</template>
+
+<script>
+import Page from '@/components/page/Page'
+import CategpryNav from '@/components/page/CategoryNav'
+
+export default {
+  components: { Page, CategpryNav }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Category from '@/components/product/Category'
 import Product from '@/components/product/Product'
+import Cart from '@/components/product/Cart'
 import Shipping from '@/components/order/Shipping'
 import Order from '@/components/order/Order'
 
@@ -25,6 +26,11 @@ export default new Router({
       path: '/products/:product',
       name: 'Product',
       component: Product
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     },
     {
       path: '/shipping',
