c8393472d9009cbef5b87f5ea6b597e19fcb1baa
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-01 14:21:48 -0600

    Added FontAwesome component to Vue with name font-awesome

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import FontAwesome from './font-awesome'
 import router from './router'
 import store from './state'
 import api from './api'
 
 Vue.config.productionTip = false
+
+Vue.component('font-awesome', FontAwesome)
 
 /* eslint-disable no-new */
 new Vue({
