b4c16a3cc25ca742c915c86249c737900c95994a
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-02 08:25:37 -0600

    Added faTrashAlt icon to font awesome library

--- src/font-awesome/index.js
+++ src/font-awesome/index.js
@@ -1,8 +1,12 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import {
+  faShoppingCart,
+  faTrashAlt
+} from '@fortawesome/free-solid-svg-icons'
 
 library.add(faShoppingCart)
+library.add(faTrashAlt)
 
 export default FontAwesomeIcon
