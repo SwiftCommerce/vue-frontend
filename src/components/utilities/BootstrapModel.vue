79e0c5b29d441c29a38273be34d3399cf43d1901
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-02 09:26:08 -0600

    Created BootstrapModel component

---
+++ src/components/utilities/BootstrapModel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="modal fade" id="bootstrapModel" tabindex="-1" role="dialog" aria-labelledby="bootstrapModel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="bootstrapModel">{{ title }}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <slot></slot>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" @click="$emit('accepted')">{{ acceptTitle }}</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    acceptTitle: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
