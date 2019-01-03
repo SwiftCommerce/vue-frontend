02d69a447a3d561fd806c56add5795232069a287
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-03 16:18:13 -0600

    Added Stripe credit card form to #stripe card in Payment component

--- src/components/order/Payment.vue
+++ src/components/order/Payment.vue
@@ -4,7 +4,7 @@
     <div class="card" id="paypal">
       <div class="card-header" id="paypal-heading">
         <h2 class="mb-0">
-        <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapse-paypal" aria-expanded="false" aria-controls="collapse-paypal">
+        <button @click="hideAll()" class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapse-paypal" aria-expanded="false" aria-controls="collapse-paypal">
           Paypal
         </button>
       </h2>
@@ -20,7 +20,7 @@
     <div class="card" id="stripe">
       <div class="card-header" id="stripe-heading">
         <h2 class="mb-0">
-          <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapse-stripe" aria-expanded="false" aria-controls="collapse-stripe">
+          <button @click="hideAll()" class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapse-stripe" aria-expanded="false" aria-controls="collapse-stripe">
             Stripe
          </button>
         </h2>
@@ -28,10 +28,62 @@
 
       <div id="collapse-stripe" class="collapse" aria-labelledby="stripe-heading" data-parent="#stripe">
         <div class="card-body">
-          Payment inputs here.
+          <form action="/charge" method="post" id="payment-form">
+            <div class="form-row">
+              <label for="card-element">
+                Credit or debit card
+              </label>
+              <div id="stripe-card-element">
+                <!-- A Stripe Element will be inserted here. -->
+              </div>
+
+              <!-- Used to display Element errors. -->
+              <div id="card-errors" role="alert"></div>
+            </div>
+
+            <button class="btn">Submit Payment</button>
+          </form>
         </div>
       </div>
     </div>
 
   </div>
 </template>
+
+<script>
+export default {
+  mounted: function () {
+    this.configureStripe()
+  },
+  methods: {
+    configureStripe: function () {
+      /* eslint-disable no-undef */
+      var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx')
+      var elements = stripe.elements()
+
+      var style = {
+        base: {
+          fontSize: '16px',
+          color: '#32325d'
+        }
+      }
+
+      var card = elements.create('card', { style: style })
+      card.mount('#stripe-card-element')
+    },
+
+    hideAll: function () {
+      let cards = document.getElementsByClassName('collapse')
+      Array.from(cards).forEach(function (card) {
+        card.classList.remove('show')
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#stripe-card-element {
+  width: 80%;
+}
+</style>
