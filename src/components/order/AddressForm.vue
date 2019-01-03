33a5834fcc3b94868acc44fb47309ef3e95cc98a
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-03 10:39:02 -0600

    Added form validation to AddressForm component

--- src/components/order/AddressForm.vue
+++ src/components/order/AddressForm.vue
@@ -1,6 +1,6 @@
 <template>
-  <form>
-      <form-element name="email" required>Email Address:</form-element>
+  <form id="address-form" class="needs-validation" novalidate>
+      <form-element name="email" type="email" required>Email Address:</form-element>
       <form-element name="firstname" required>First Name:</form-element>
       <form-element name="lastname" required>Last Name:</form-element>
       <form-element name="company">Company:</form-element>
@@ -10,9 +10,9 @@
       <form-element name="state" required>State/Province:</form-element>
       <form-element name="zip" required>Zip/Postal Code:</form-element>
       <form-element name="country" required>Country:</form-element>
-      <form-element name="phone_number">Phone Number:</form-element>
+      <form-element name="phone_number" type="tel">Phone Number:</form-element>
 
-      <button v-if="next" type="submit" class="btn btn-info" @click="submit()">Next</button>
+      <button v-if="next" type="submit" class="btn btn-info" @click="submit()" onsubmit="return false">Next</button>
     </form>
 </template>
 
@@ -25,10 +25,18 @@ export default {
     next: { type: String }
   },
   methods: {
-    submit: function () {
+    submit: function (event) {
+      this.$router.replace('/shipping')
+      if (!this.validateFields()) { return }
       if (this.next) {
         this.$router.push({name: this.next})
       }
+    },
+    validateFields: function () {
+      var form = document.getElementById('address-form')
+
+      form.classList.add('was-validated')
+      return form.checkValidity()
     }
   }
 }
