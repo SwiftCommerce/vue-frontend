76a47e61c522ebc41dca2758c47eac869b2f96a2
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-03 10:38:22 -0600

    Added validation failure message to FormElement component

--- src/components/utilities/FormElement.vue
+++ src/components/utilities/FormElement.vue
@@ -1,10 +1,17 @@
 <template>
-    <div class="form-group" :id="name">
-        <label :for="name"><slot></slot></label>
+  <div class="form-group" :id="name">
+    <label :for="name"><slot></slot></label>
 
-        <input v-if="this.required" class="form-control" :type="type" :name="name" :id="name" required>
-        <input v-else class="form-control" :type="type" :name="name" :id="name">
+    <div v-if="this.required" class="input-group">
+      <input v-if="this.required" class="form-control" :type="type" :name="name" :id="name" required>
+      <div class="invalid-feedback">
+        <p>The given value is not valid</p>
+      </div>
     </div>
+    <div v-else class="input-group">
+      <input class="form-control" :type="type" :name="name" :id="name">
+    </div>
+  </div>
 </template>
 
 <script>
