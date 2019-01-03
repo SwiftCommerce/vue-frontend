554bb037f1d1c842d921ba82dbe9562972c58bcc
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-03 11:24:18 -0600

    Display cart item information in Order component #cart div

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -5,7 +5,17 @@
           <payment></payment>
         </div>
         <div id="cart" class="col-6">
-          Cart
+          <ul class="list-group">
+            <li v-for="item in $store.state.cart" :key="item.product.sku" class="order-cart-item row list-group-item">
+              <div class="col-3 product-image-container">
+                <img class="product-image" :src="imageURL(item.product)" />
+              </div>
+              <div class="col-9 product-information">
+                <p class="produc-name">{{ item.product.name }}</p>
+                <p>Quantity: {{ item.count }}</p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
   </page>
@@ -16,12 +26,40 @@ import Page from '@/components/page/Page.vue'
 import Payment from '@/components/order/Payment.vue'
 
 export default {
-  components: { Page, Payment }
+  components: { Page, Payment },
+  data: function () {
+    return {
+      defaultImage: require('@/assets/fa-image.png')
+    }
+  },
+  methods: {
+    imageURL: function (product) {
+      return product.attributes.filter((attr) => attr.name === 'image')[0] || this.defaultImage
+    }
+  }
 }
 </script>
 
 <style>
-#cart {
-  background-color: rgb(233, 233, 233);
+#cart-header {
+  background-color: #f7f7f7;
 }
+
+.product-image-container {
+  float: left;
+}
+
+.product-information {
+  float: right;
+}
+
+.product-image {
+  width: 5em;
+  height: 5em;
+}
+
+.produc-name {
+  font-size: 1.25em;
+}
+
 </style>
