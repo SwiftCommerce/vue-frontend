ab877f15211ef37bdd014861b0562639ff31db82
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-07 09:39:28 -0600

    Removed CC label and submit button from Payment #stripe card

--- src/components/order/Payment.vue
+++ src/components/order/Payment.vue
@@ -30,9 +30,6 @@
         <div class="card-body">
           <form action="/charge" method="post" id="payment-form">
             <div class="form-row">
-              <label for="card-element">
-                Credit or debit card
-              </label>
               <div id="stripe-card-element">
                 <!-- A Stripe Element will be inserted here. -->
               </div>
@@ -40,8 +37,6 @@
               <!-- Used to display Element errors. -->
               <div id="card-errors" role="alert"></div>
             </div>
-
-            <button class="btn">Submit Payment</button>
           </form>
         </div>
       </div>
