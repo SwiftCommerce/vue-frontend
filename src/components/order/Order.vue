7a9c7ed56c0c6f1aba6be4a4a2237472764cdc1d
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-08 16:33:25 -0600

    Added loading indicator and error alert to Order component

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,42 +1,59 @@
 <template>
-  <page name="Order">
-    <div class="row">
-        <div id="order-info" class="col-6">
-          <div id="payment">
-            <payment></payment>
-          </div>
-          <div id="address-form">
-            <hr />
-
-            <input v-model="useShippingAddress" type="checkbox" name="hide-address-form"/>
-            <label for="hide-address-form">Use shipping address as billing address</label>
-
-            <address-form v-if="!useShippingAddress" state-mutation="billingAddress"></address-form>
-          </div>
-          <div id="order-comment">
-            <hr />
-            <label for="comment">Leave any comments about the order here:</label>
-            <textarea v-model="orderComment" class="form-control" name="comment" id="comment"/>
-          </div>
-          <div id="place-order">
-            <hr />
-            <button type="submit" class="btn btn-primary" @click="createOrder()">Place Order</button>
-          </div>
-        </div>
-        <div id="cart" class="col-6">
-          <ul class="list-group">
-            <li v-for="item in $store.state.cart" :key="item.product.sku" class="order-cart-item row list-group-item">
-              <div class="col-3 product-image-container">
-                <img class="product-image" :src="imageURL(item.product)" />
-              </div>
-              <div class="col-9 product-information">
-                <p class="produc-name">{{ item.product.name }}</p>
-                <p>Quantity: {{ item.count }}</p>
-              </div>
-            </li>
-          </ul>
+  <page id="order-page" name="Order">
+    
+    <div v-if="showLoader" id="loader">
+      <div class="centered text-center">
+        <div id="spinner" class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span>
         </div>
       </div>
+    </div>
+
+    <div v-if="error" class="row">
+      <div class="col-12">
+        <div class="alert alert-danger" role="alert">
+          <p>{{ error }}</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div id="order-info" class="col-6">
+        <div id="payment">
+          <payment></payment>
+        </div>
+        <div id="address-form">
+          <hr />
+
+          <input v-model="useShippingAddress" type="checkbox" name="hide-address-form"/>
+          <label for="hide-address-form">Use shipping address as billing address</label>
+
+          <address-form v-if="!useShippingAddress" state-mutation="billingAddress"></address-form>
+        </div>
+        <div id="order-comment">
+          <hr />
+          <label for="comment">Leave any comments about the order here:</label>
+          <textarea v-model="orderComment" class="form-control" name="comment" id="comment"/>
+        </div>
+        <div id="place-order">
+          <hr />
+          <button type="submit" class="btn btn-primary" @click="createOrder()">Place Order</button>
+        </div>
+      </div>
+      <div id="cart" class="col-6">
+        <ul class="list-group">
+          <li v-for="item in $store.state.cart" :key="item.product.sku" class="order-cart-item row list-group-item">
+            <div class="col-3 product-image-container">
+              <img class="product-image" :src="imageURL(item.product)" />
+            </div>
+            <div class="col-9 product-information">
+              <p class="produc-name">{{ item.product.name }}</p>
+              <p>Quantity: {{ item.count }}</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </page>
 </template>
 
@@ -51,7 +68,10 @@ export default {
     return {
       useShippingAddress: true,
       orderComment: null,
-      defaultImage: require('@/assets/fa-image.png')
+      defaultImage: require('@/assets/fa-image.png'),
+
+      error: null,
+      showLoader: false
     }
   },
   methods: {
@@ -73,6 +93,8 @@ export default {
       }
     },
     createOrder: function () {
+      this.showLoader = true
+
       var items = this.$store.state.cart.map(function (item) {
         return {
           productID: item.product.id,
@@ -116,9 +138,12 @@ export default {
       this.$api.orders.defaults.headers.common['Authorization'] = this.$store.state.authToken
       this.$api.orders.post('', body).then((response) => {
         this.$store.commit('authToken', response.data.authToken)
-        this.$router.push({name: 'OrderSuccess'})
+        //return Payment.methods.createOrder(response.data.id)
+      }).then(() => {
+        this.showLoader = false
       }).catch((error) => {
-        alert('Error: ', error)
+        this.error = error.message
+        this.showLoader = false
       })
     }
   }
@@ -126,6 +151,31 @@ export default {
 </script>
 
 <style>
+#loader {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+
+  opacity: 0.8;
+  background-color: white;
+  z-index: 1;
+}
+
+.centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+
+#spinner {
+  height: 5em;
+  width: 5em;
+}
+
 #cart-header {
   background-color: #f7f7f7;
 }
