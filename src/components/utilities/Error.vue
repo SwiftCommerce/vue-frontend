feed2137ddb35fa7f8bd208b635823e71b8975d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-10 16:46:33 -0600

    Created Error component

---
+++ src/components/utilities/Error.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="error" id="error-alert" class="alert alert-danger" role="alert">
+    {{ error.message }}
+    <button @click="hide()" type="button" class="close" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    watch: {
+      type: Error
+    }
+  },
+  watch: {
+    'watch': 'show'
+  },
+  data: function () {
+    return {
+      error: null
+    }
+  },
+  methods: {
+    hide: function () {
+      this.error = null
+    },
+    show: function (value, old) {
+      console.log(value)
+      this.error = value
+    }
+  }
+}
+</script>
