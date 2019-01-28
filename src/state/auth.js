8dca5617b6d511706badad8d0b25ed7e03bbd395
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-01-28 12:06:21 -0600

    Store auth and refresh tokens in local storage

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,12 +1,16 @@
 export default {
   namespaced: true,
   state: {
-    token: null,
+    token: localStorage.getItem('auth/token'),
+    refresh: localStorage.getItem('auth/refresh'),
     isAuthenticated: false
   },
   mutations: {
-    token (state, token) {
-      state.token = token
+    token (_, token) {
+      localStorage.setItem('auth/token', token)
+    },
+    refresh (_, refresh) {
+      localStorage.setItem('auth/refresh', refresh)
     },
     isAuthenticated: function (state, is) {
       state.isAuthenticated = is
