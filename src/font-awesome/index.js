9fd8239da6c83df8786102b44008ec59062e8fb6
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-06 12:55:51 -0600

    Added angle icon to font-awesome library for pagination nav

--- src/font-awesome/index.js
+++ src/font-awesome/index.js
@@ -2,17 +2,25 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import {
+  faAngleDoubleRight,
+  faAngleDoubleLeft,
   faShoppingCart,
   faSignOutAlt,
   faSignInAlt,
+  faAngleRight,
+  faAngleLeft,
   faTrashAlt,
   faHistory,
   faCheck
 } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faAngleDoubleRight)
+library.add(faAngleDoubleLeft)
 library.add(faShoppingCart)
 library.add(faSignOutAlt)
 library.add(faSignInAlt)
+library.add(faAngleRight)
+library.add(faAngleLeft)
 library.add(faTrashAlt)
 library.add(faHistory)
 library.add(faCheck)
