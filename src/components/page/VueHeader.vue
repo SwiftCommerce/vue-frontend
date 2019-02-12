c18026434848a7a4aaf48bdba44ea992e2932a39
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-12 16:21:35 -0600

    Added icon to header that links back to home

--- src/components/page/VueHeader.vue
+++ src/components/page/VueHeader.vue
@@ -1,18 +1,53 @@
 <template>
   <header>
     <div class="row">
-      <h1 class="col"><slot></slot></h1>
+      <div id="logo" class="col-1">
+        <router-link :to="{ name: 'HelloWorld' }"><img :src="logo" /></router-link>
+      </div>
+      <h1 class="col-auto mx-auto"><slot></slot></h1>
+      <div style="width: 8em; padding: 0 1rem;"></div>
     </div>
   </header>
 </template>
 
+<script>
+export default {
+  data: function () {
+    return {
+      logo: require('@/assets/swift_commerce_text.svg')
+    }
+  }
+}
+</script>
+
 <style>
 header {
-  padding-top: 1em;
+  padding: 1em 3em 0.5em;
+  color: white;
+  background-color: #264568;
 }
 
-h1 {
+header > h1 {
   font: 2em;
   text-align: center;
+}
+
+#logo > a {
+  height: 3em;
+  width: 14.41em;
+
+  transition: 0.4s;
+  clip-path: inset(-100% 78% -100% -10%);
+}
+
+#logo > a > img {
+  height: 3em;
+  width: 14.41em;
+}
+
+@media (min-width: 992px) {
+  #logo > a:hover {
+    clip-path: inset(-100% -10% -100% -10%);
+  }
 }
 </style>
