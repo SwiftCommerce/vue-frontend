83540d27526c03d5dfec88c39525ebb0ce528aa9
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-13 07:51:17 -0600

    Fixed product information placement on small screen sizes with size specific bootstrap columns

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,5 +1,5 @@
 <template>
-  <page :name="product.name ? product.name : ''">
+  <page name="Food">
     <category-nav></category-nav>
 
     <div v-if="loading">
@@ -19,25 +19,27 @@
       </div>
       <div id="product-modifications" class="col-12 col-lg-6">
         <div id="product-info" class="row">
-          <h5 id="price" class="col-6">{{ price || 'Not for Sale' }}</h5>
-          <p v-if="manufacturer" class="col-6 text-right">By {{ manufacturer }}</p>
+          <h5 id="name" class="col-8">{{ product.name }}</h5>
+          <h5 id="price" class="col-4 text-right">{{ price || 'Not for Sale' }}</h5>
+          <p v-if="manufacturer" class="col-12"> by {{ manufacturer }}</p>
           <hr class="col-12"/>
-          <p>{{ product.description }}</p>
+          <p class="col-12">{{ product.description }}</p>
         </div>
         <div id="order" class="row">
-          <div class="input-group mb-3">
+          <div class="input-group mb-3 col-12">
             <div class="input-group-prepend">
               <span class="input-group-text" id="basic-addon1">Quantity</span>
             </div>
             <input v-model="productCount" type="number" class="form-control" placeholder="Qty" aria-label="Quantity" aria-describedby="basic-addon1">
           </div>
+          <div class="col-12">
+            <span>
+              <button v-if="price && productCount > 0" id="add-to-cart" type="button" class="btn btn-primary" @click="addToCart()">Add to Cart</button>
+              <button v-else type="button" class="btn btn-primary" disabled>Add to Cart</button>
 
-          <span>
-            <button v-if="price && productCount > 0" id="add-to-cart" type="button" class="btn btn-primary" @click="addToCart()">Add to Cart</button>
-            <button v-else type="button" class="btn btn-primary" disabled>Add to Cart</button>
-
-            <font-awesome id="added-to-cart" class="text-success" style="display: none;" icon="check" />
-          </span>
+              <font-awesome id="added-to-cart" class="text-success" style="display: none;" icon="check" />
+            </span>
+          </div>
         </div>
       </div>
     </div>
